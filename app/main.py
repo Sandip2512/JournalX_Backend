504bbd7946c9f9636be9a003e51b06a0859ce45f
Sandip Salunkhe <90053386+Sandip2512@users.noreply.github.com>
2025-12-17 05:16:54 +0530

    Temp: Allow ALL CORS origins

--- app/main.py
+++ app/main.py
@@ -45,12 +45,14 @@ origins = [
     "http://127.0.0.1:8080",
     "http://192.168.1.3:8080",
     "https://journal-x-frontend.vercel.app",
-    "https://journalx-trading.vercel.app"
+    "https://journalx-trading.vercel.app",
+    "https://journal-x-frontend-git-main-sandip2512s-projects.vercel.app",
+    "https://*.vercel.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
